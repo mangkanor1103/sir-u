29a437af5db10f823975fa802e991aa8827c16f1
Kian <kianr664@gmail.com>
2025-02-18 19:50:49 +0800

    db

--- db/votingsystem5.sql
+++ db/votingsystem5.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 26, 2024 at 04:12 AM
+-- Generation Time: Feb 18, 2025 at 11:09 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -48,6 +48,30 @@ INSERT INTO `admin` (`id`, `election_id`, `username`, `password`, `firstname`, `
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `cache`
+--
+
+CREATE TABLE `cache` (
+  `key` varchar(255) NOT NULL,
+  `value` mediumtext NOT NULL,
+  `expiration` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cache_locks`
+--
+
+CREATE TABLE `cache_locks` (
+  `key` varchar(255) NOT NULL,
+  `owner` varchar(255) NOT NULL,
+  `expiration` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `candidates`
 --
 
@@ -66,12 +90,11 @@ CREATE TABLE `candidates` (
 --
 
 INSERT INTO `candidates` (`id`, `election_id`, `position_id`, `firstname`, `lastname`, `photo`, `platform`) VALUES
-(4, 1, 4, 'Rodriguez', 'Kian A.', '9101e301-ce8a-4ece-a819-81ddac01caec-1664288564642.jpg', '4t34t34y3'),
-(5, 1, 3, 'asas', 'kian1103', '36862-w740.jpg', 'e5y4y4eu4'),
-(6, 1, 3, 'yana', 'db', '1_thCB4VzqsRapqFCHC8w7EQ.jpg', '4yy54u'),
-(7, 1, 3, 'asas', 'kian', '320386531_517473353478779_9191284672055013034_n.jpg', 'y4y4y4yy'),
-(8, 1, 4, 'tktk', 'ktyktgk', '998381_432429093530620_919998537_a.jpg', 'yktktk'),
-(9, 1, 5, 'wgewgw', 'egegwg', 'hqdefault (1).jpg', 'etgwggwgw');
+(11, 1, 4, 'asfaf', 'afffafaf', 'uploads/OIP.jfif', 'faf'),
+(13, 1, 3, 'asfaf', 'afffafaf', 'uploads/OIP.jfif', 'htjrykjtfkytfk'),
+(14, 1, 6, 'rerhehe', 'rhdhdhs', 'uploads/OIP.jfif', 'wfafafa'),
+(15, 1, 3, 'kian', 'reeeyeye', 'uploads/OIP.jfif', 'hdjd'),
+(16, 1, 3, 'sfwetwtw', 'ryteyey', 'uploads/458804344_1108143657387022_7368374457459692655_n.jpg', 'ryeyey');
 
 -- --------------------------------------------------------
 
@@ -82,16 +105,122 @@ INSERT INTO `candidates` (`id`, `election_id`, `position_id`, `firstname`, `last
 CREATE TABLE `elections` (
   `id` int(11) NOT NULL,
   `name` varchar(100) NOT NULL,
-  `password` varchar(60) NOT NULL
+  `election_code` varchar(60) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Dumping data for table `elections`
 --
 
-INSERT INTO `elections` (`id`, `name`, `password`) VALUES
+INSERT INTO `elections` (`id`, `name`, `election_code`) VALUES
 (1, 'General Election 2024', '$2y$10$samplehashedpassword'),
-(2, 'Student Council Election 2024', '$2y$10$samplehashedpassword2');
+(2, 'Student Council Election 2024', '$2y$10$samplehashedpassword2'),
+(3, 'usg election', '7xbKFkUZTM');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `failed_jobs`
+--
+
+CREATE TABLE `failed_jobs` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `uuid` varchar(255) NOT NULL,
+  `connection` text NOT NULL,
+  `queue` text NOT NULL,
+  `payload` longtext NOT NULL,
+  `exception` longtext NOT NULL,
+  `failed_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `feedback`
+--
+
+CREATE TABLE `feedback` (
+  `id` int(11) NOT NULL,
+  `voter_id` int(11) NOT NULL,
+  `election_id` int(11) NOT NULL,
+  `feedback` text NOT NULL,
+  `created_at` datetime DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `feedback`
+--
+
+INSERT INTO `feedback` (`id`, `voter_id`, `election_id`, `feedback`, `created_at`) VALUES
+(1, 2, 1, 'jh,.h', '2024-10-10 07:31:25');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `jobs`
+--
+
+CREATE TABLE `jobs` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `queue` varchar(255) NOT NULL,
+  `payload` longtext NOT NULL,
+  `attempts` tinyint(3) UNSIGNED NOT NULL,
+  `reserved_at` int(10) UNSIGNED DEFAULT NULL,
+  `available_at` int(10) UNSIGNED NOT NULL,
+  `created_at` int(10) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `job_batches`
+--
+
+CREATE TABLE `job_batches` (
+  `id` varchar(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `total_jobs` int(11) NOT NULL,
+  `pending_jobs` int(11) NOT NULL,
+  `failed_jobs` int(11) NOT NULL,
+  `failed_job_ids` longtext NOT NULL,
+  `options` mediumtext DEFAULT NULL,
+  `cancelled_at` int(11) DEFAULT NULL,
+  `created_at` int(11) NOT NULL,
+  `finished_at` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `migrations`
+--
+
+CREATE TABLE `migrations` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `migration` varchar(255) NOT NULL,
+  `batch` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `migrations`
+--
+
+INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
+(1, '0001_01_01_000000_create_users_table', 1),
+(2, '0001_01_01_000001_create_cache_table', 1),
+(3, '0001_01_01_000002_create_jobs_table', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `password_reset_tokens`
+--
+
+CREATE TABLE `password_reset_tokens` (
+  `email` varchar(255) NOT NULL,
+  `token` varchar(255) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -100,7 +229,7 @@ INSERT INTO `elections` (`id`, `name`, `password`) VALUES
 --
 
 CREATE TABLE `positions` (
-  `id` int(11) NOT NULL,
+  `position_id` int(11) NOT NULL,
   `election_id` int(11) NOT NULL,
   `description` varchar(50) NOT NULL,
   `max_vote` int(11) NOT NULL,
@@ -111,10 +240,62 @@ CREATE TABLE `positions` (
 -- Dumping data for table `positions`
 --
 
-INSERT INTO `positions` (`id`, `election_id`, `description`, `max_vote`, `priority`) VALUES
+INSERT INTO `positions` (`position_id`, `election_id`, `description`, `max_vote`, `priority`) VALUES
 (3, 1, 'vice president', 5, 2),
 (4, 1, 'president', 5, 1),
-(5, 1, 'secretary', 7, 3);
+(6, 1, 'sec', 1, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sessions`
+--
+
+CREATE TABLE `sessions` (
+  `id` varchar(255) NOT NULL,
+  `user_id` bigint(20) UNSIGNED DEFAULT NULL,
+  `ip_address` varchar(45) DEFAULT NULL,
+  `user_agent` text DEFAULT NULL,
+  `payload` longtext NOT NULL,
+  `last_activity` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `student`
+--
+
+CREATE TABLE `student` (
+  `id` int(11) NOT NULL,
+  `student_id` varchar(20) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `department` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `student`
+--
+
+INSERT INTO `student` (`id`, `student_id`, `name`, `department`) VALUES
+(1, 'MBC2022-0197', 'Kian Rodriguez', 'IT Department');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `email_verified_at` timestamp NULL DEFAULT NULL,
+  `password` varchar(255) NOT NULL,
+  `remember_token` varchar(100) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -133,7 +314,17 @@ CREATE TABLE `voters` (
 --
 
 INSERT INTO `voters` (`id`, `election_id`, `voters_id`) VALUES
-(2, 1, 'ktVGmlwiofvC7cZ');
+(2, 1, 'ktVGmlwiofvC7cZ'),
+(3, 1, 'lofy0JcdGO'),
+(4, 1, 'ijqY7xubGw'),
+(5, 1, 'nqJ0QrxiqU'),
+(6, 1, 'NrCi4Q9yze'),
+(7, 1, '0kVqoSbPZR'),
+(8, 1, 'm8pwonNEyP'),
+(9, 1, 'fKG9WWq4sL'),
+(10, 3, 'ZwsLyhfKbN'),
+(11, 1, 'EwKRVINUtc'),
+(12, 1, '49BewO5sVc');
 
 -- --------------------------------------------------------
 
@@ -146,17 +337,33 @@ CREATE TABLE `votes` (
   `election_id` int(11) NOT NULL,
   `voters_id` int(11) NOT NULL,
   `candidate_id` int(11) NOT NULL,
-  `position_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+  `position_id` int(11) NOT NULL,
+  `timestamp` datetime DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `votes`
 --
 
-INSERT INTO `votes` (`id`, `election_id`, `voters_id`, `candidate_id`, `position_id`) VALUES
-(1, 1, 2, 5, 3),
-(2, 1, 2, 4, 4),
-(3, 1, 2, 9, 5);
+INSERT INTO `votes` (`id`, `election_id`, `voters_id`, `candidate_id`, `position_id`, `timestamp`) VALUES
+(13, 1, 8, 11, 4, '2024-10-03 20:07:00'),
+(14, 1, 8, 14, 6, '2024-10-03 20:07:00'),
+(15, 1, 8, 13, 3, '2024-10-03 20:07:00'),
+(16, 1, 4, 11, 4, '2024-10-03 20:12:41'),
+(17, 1, 4, 14, 6, '2024-10-03 20:12:41'),
+(18, 1, 4, 13, 3, '2024-10-03 20:12:41'),
+(19, 1, 2, 11, 4, '2024-10-03 20:16:07'),
+(20, 1, 2, 14, 6, '2024-10-03 20:16:07'),
+(21, 1, 2, 13, 3, '2024-10-03 20:16:07'),
+(22, 1, 9, 11, 4, '2024-10-04 13:36:51'),
+(23, 1, 9, 14, 6, '2024-10-04 13:36:51'),
+(24, 1, 9, 13, 3, '2024-10-04 13:36:51'),
+(25, 1, 6, 11, 4, '2024-10-10 08:03:40'),
+(26, 1, 6, 14, 6, '2024-10-10 08:03:40'),
+(27, 1, 6, 15, 3, '2024-10-10 08:03:40'),
+(28, 1, 12, 11, 4, '2025-01-31 18:10:14'),
+(29, 1, 12, 14, 6, '2025-01-31 18:10:15'),
+(30, 1, 12, 13, 3, '2025-01-31 18:10:24');
 
 --
 -- Indexes for dumped tables
@@ -170,11 +377,24 @@ ALTER TABLE `admin`
   ADD KEY `election_id` (`election_id`);
 
 --
+-- Indexes for table `cache`
+--
+ALTER TABLE `cache`
+  ADD PRIMARY KEY (`key`);
+
+--
+-- Indexes for table `cache_locks`
+--
+ALTER TABLE `cache_locks`
+  ADD PRIMARY KEY (`key`);
+
+--
 -- Indexes for table `candidates`
 --
 ALTER TABLE `candidates`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `election_id` (`election_id`);
+  ADD KEY `election_id` (`election_id`),
+  ADD KEY `position_id` (`position_id`);
 
 --
 -- Indexes for table `elections`
@@ -183,11 +403,72 @@ ALTER TABLE `elections`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `failed_jobs`
+--
+ALTER TABLE `failed_jobs`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `failed_jobs_uuid_unique` (`uuid`);
+
+--
+-- Indexes for table `feedback`
+--
+ALTER TABLE `feedback`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `voter_id` (`voter_id`),
+  ADD KEY `election_id` (`election_id`);
+
+--
+-- Indexes for table `jobs`
+--
+ALTER TABLE `jobs`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `jobs_queue_index` (`queue`);
+
+--
+-- Indexes for table `job_batches`
+--
+ALTER TABLE `job_batches`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `migrations`
+--
+ALTER TABLE `migrations`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `password_reset_tokens`
+--
+ALTER TABLE `password_reset_tokens`
+  ADD PRIMARY KEY (`email`);
+
+--
 -- Indexes for table `positions`
 --
 ALTER TABLE `positions`
-  ADD PRIMARY KEY (`id`),
+  ADD PRIMARY KEY (`position_id`),
   ADD KEY `election_id` (`election_id`);
+
+--
+-- Indexes for table `sessions`
+--
+ALTER TABLE `sessions`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `sessions_user_id_index` (`user_id`),
+  ADD KEY `sessions_last_activity_index` (`last_activity`);
+
+--
+-- Indexes for table `student`
+--
+ALTER TABLE `student`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `users_email_unique` (`email`);
 
 --
 -- Indexes for table `voters`
@@ -201,7 +482,10 @@ ALTER TABLE `voters`
 --
 ALTER TABLE `votes`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `election_id` (`election_id`);
+  ADD KEY `election_id` (`election_id`),
+  ADD KEY `voters_id` (`voters_id`),
+  ADD KEY `candidate_id` (`candidate_id`),
+  ADD KEY `position_id` (`position_id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -217,31 +501,67 @@ ALTER TABLE `admin`
 -- AUTO_INCREMENT for table `candidates`
 --
 ALTER TABLE `candidates`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- AUTO_INCREMENT for table `elections`
 --
 ALTER TABLE `elections`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `failed_jobs`
+--
+ALTER TABLE `failed_jobs`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `feedback`
+--
+ALTER TABLE `feedback`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `jobs`
+--
+ALTER TABLE `jobs`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `migrations`
+--
+ALTER TABLE `migrations`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `positions`
 --
 ALTER TABLE `positions`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `position_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `student`
+--
+ALTER TABLE `student`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `voters`
 --
 ALTER TABLE `voters`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT for table `votes`
 --
 ALTER TABLE `votes`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
 
 --
 -- Constraints for dumped tables
@@ -260,6 +580,13 @@ ALTER TABLE `candidates`
   ADD CONSTRAINT `candidates_ibfk_1` FOREIGN KEY (`election_id`) REFERENCES `elections` (`id`);
 
 --
+-- Constraints for table `feedback`
+--
+ALTER TABLE `feedback`
+  ADD CONSTRAINT `feedback_ibfk_1` FOREIGN KEY (`voter_id`) REFERENCES `voters` (`id`),
+  ADD CONSTRAINT `feedback_ibfk_2` FOREIGN KEY (`election_id`) REFERENCES `elections` (`id`);
+
+--
 -- Constraints for table `positions`
 --
 ALTER TABLE `positions`
@@ -275,7 +602,10 @@ ALTER TABLE `voters`
 -- Constraints for table `votes`
 --
 ALTER TABLE `votes`
-  ADD CONSTRAINT `votes_ibfk_1` FOREIGN KEY (`election_id`) REFERENCES `elections` (`id`);
+  ADD CONSTRAINT `votes_ibfk_1` FOREIGN KEY (`election_id`) REFERENCES `elections` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `votes_ibfk_2` FOREIGN KEY (`voters_id`) REFERENCES `voters` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `votes_ibfk_3` FOREIGN KEY (`candidate_id`) REFERENCES `candidates` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `votes_ibfk_4` FOREIGN KEY (`position_id`) REFERENCES `positions` (`position_id`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
