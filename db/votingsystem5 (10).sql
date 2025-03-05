6353c569c04efe2c1a32b610088523c39255c463
Kian <kianr664@gmail.com>
2025-03-05 21:09:21 +0800

    db

--- db/votingsystem5 (10).sql
+++ db/votingsystem5 (10).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 04, 2025 at 04:26 PM
+-- Generation Time: Mar 05, 2025 at 02:08 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -82,17 +82,16 @@ CREATE TABLE `candidates` (
   `lastname` varchar(30) NOT NULL,
   `photo` varchar(150) NOT NULL,
   `platform` text NOT NULL,
-  `partylists_id` int(11) DEFAULT NULL
+  `partylists_id` int(11) DEFAULT NULL,
+  `partylist_id` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Dumping data for table `candidates`
 --
 
-INSERT INTO `candidates` (`id`, `election_id`, `position_id`, `firstname`, `lastname`, `photo`, `platform`, `partylists_id`) VALUES
-(24, 12, 12, 'Kian', 'A. Rodriguez', 'uploads/475712464_1547996512641952_3911195512472841382_n.jpg', 'wdfwqfdq', NULL),
-(25, 12, 1, 'Kian', 'A. Rodriguez', 'uploads/detect.jpg', 'wdfwqfdq', NULL),
-(26, 12, 1, 'Kian', 'A. Rodriguez', 'uploads/c69fa31c-03f2-44ce-ac2d-5851aa08bf62.jpg', 'wdfwqfdq', NULL);
+INSERT INTO `candidates` (`id`, `election_id`, `position_id`, `firstname`, `lastname`, `photo`, `platform`, `partylists_id`, `partylist_id`) VALUES
+(29, 13, 2, 'ad', 'asd', 'uploads/4c1eb3fb-bf37-40f2-a495-f1d061784156.jpg', '2r2r2', NULL, 3);
 
 -- --------------------------------------------------------
 
@@ -111,7 +110,7 @@ CREATE TABLE `elections` (
 --
 
 INSERT INTO `elections` (`id`, `name`, `election_code`) VALUES
-(12, 'usg election', 'tRwKihXZO6');
+(13, 'usg election', 'WRKv4kUSVu');
 
 -- --------------------------------------------------------
 
@@ -147,7 +146,8 @@ CREATE TABLE `feedback` (
 --
 
 INSERT INTO `feedback` (`id`, `election_id`, `feedback`, `created_at`) VALUES
-(1, 12, 'qwewq', '2025-03-04 15:25:12');
+(2, 13, 'fwegfwg', '2025-03-05 12:47:19'),
+(3, 13, 'efwgwge', '2025-03-05 12:48:12');
 
 -- --------------------------------------------------------
 
@@ -158,20 +158,20 @@ INSERT INTO `feedback` (`id`, `election_id`, `feedback`, `created_at`) VALUES
 CREATE TABLE `history` (
   `id` int(11) NOT NULL,
   `election_title` varchar(255) NOT NULL,
-  `deleted_at` datetime DEFAULT current_timestamp(),
+  `deleted_at` datetime NOT NULL,
   `candidates` text DEFAULT NULL,
   `voters` text DEFAULT NULL,
   `votes` text DEFAULT NULL,
-  `positions` text DEFAULT NULL
+  `positions` text DEFAULT NULL,
+  `partylists` text DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `history`
 --
 
-INSERT INTO `history` (`id`, `election_title`, `deleted_at`, `candidates`, `voters`, `votes`, `positions`) VALUES
-(1, 'usg election', '2025-03-02 18:17:57', '22|10|Kian|A. Rodriguez|uploads/head.jpg|sqqs', '87|E245k9oHxR', '80|87|22|10|2025-03-02 18:17:19', '10|pres|2|1'),
-(2, 'usg election', '2025-03-02 18:31:58', '23|11|Kian|A. Rodriguez|uploads/sand.jpg|asfasf', '88|4V1HqF9tix', '81|88|23|11|2025-03-02 18:24:08', '11|pres|2|1');
+INSERT INTO `history` (`id`, `election_title`, `deleted_at`, `candidates`, `voters`, `votes`, `positions`, `partylists`) VALUES
+(1, 'usg election', '2025-03-05 20:37:34', '28|1|ad|asd|uploads/553838_Hollow Block picture.jpg|wfwwgw|2', '89|s72cwf9U7e;90|dp0Xn5aW3T;91|tCh7HtFfwf;92|4kWdFU8sRN;93|CCWUtuRGGO;94|aqSZlGQuzg;95|owP1VdX8s4;96|Oa97gF47YM;97|p5F1nNk1WF;98|516z4gl8VB;99|tEuoKidF1q;100|JlIiW0IqGk;101|UvnjPSRkyi;102|5otq4bWsmY;103|OZOYI8CyDq;104|pcteMPDJwS;105|48FMtyfsSU;106|y2RLS8g081;107|7nxuw3K8hB;108|DtCLgiLLBZ;109|kbEaaVuFbq;110|RBtDE34sJt;111|6Xd9zM0kD5;112|rNgoCwQfKP;113|JJla2BNGJ4;114|OnzgGxn1Po;115|ouLpXR7wds;116|ijSq3j4grB;117|U0L0zxcGou;118|HbGxM8rStG;119|F8m0oZostk;120|KGy5THJEM3;121|1lRO1Vqi1Y;122|FQmwThED4X;123|nk155DDyLq;124|YXXwF0weUF;125|Rz4z0E1iex;126|Iahd6b9Ygj;127|rbiLdEhpae;128|Ny861fNCbE;129|TEvSDqw5S9;130|oxtlyV8F97;131|6vn9B0huZt;132|hY6gDuIgR7;133|h6X9tlbqyO;134|md9cse88eq;135|dr78SYNchN;136|3yU81BFhr7;137|5yopbkPVuM;138|bZ1RByLKYw;139|YtrxQq6ZIL;140|PP8cM0bPi3;141|ba0540DnGg;142|qF4ynuUgfy;143|KaDgUDgSEc;144|rTbWoqmzLm;145|K0xASt1QvN;146|AwwDoiNyLo;147|ZrZplKnTcK;148|yr4oEJ0ANM;149|VxKcrzmlpw;150|ue1qIuPXLV;151|0tW4Fn5uTF;152|e63gbKxy0w;153|KRqlq6wwUw;154|z9QuNztfpA;155|iA99i10I81', '82|89|24|12|2025-03-02 18:43:25;83|90|25|1|2025-03-04 22:38:33;84|90|26|1|2025-03-04 22:38:33;85|91|25|1|2025-03-04 22:39:45;86|91|26|1|2025-03-04 22:39:45;87|92|25|1|2025-03-04 23:00:20;88|93|25|1|2025-03-04 23:04:55;90|94|25|1|2025-03-04 23:07:50;91|95|25|1|2025-03-04 23:16:35', '1|pres|1', '2|Republic Corp.');
 
 -- --------------------------------------------------------
 
@@ -341,7 +341,7 @@ CREATE TABLE `partylists` (
 --
 
 INSERT INTO `partylists` (`partylist_id`, `election_id`, `name`) VALUES
-(2, 12, 'Republic Corp.');
+(3, 13, 'Republic Corp.');
 
 -- --------------------------------------------------------
 
@@ -373,7 +373,7 @@ CREATE TABLE `positions` (
 --
 
 INSERT INTO `positions` (`position_id`, `election_id`, `description`, `max_vote`) VALUES
-(1, 12, 'pres', 1);
+(2, 13, 'pres', 1);
 
 -- --------------------------------------------------------
 
@@ -430,7 +430,8 @@ CREATE TABLE `students` (
 --
 
 INSERT INTO `students` (`id`, `voters_id`, `name`, `year_section`, `course`, `election_id`) VALUES
-(1, 95, 'Rodriguez, Kian, Anthony', 'bsit 3-1', 'it', 12);
+(2, 156, 'Kian A. Rodriguez', 'bsit 3-1', 'it', 13),
+(3, 157, 'Kian A. Rodriguezw', 'bsit 3-1', 'it', 13);
 
 -- --------------------------------------------------------
 
@@ -466,13 +467,21 @@ CREATE TABLE `voters` (
 --
 
 INSERT INTO `voters` (`id`, `election_id`, `voters_id`) VALUES
-(89, 12, 's72cwf9U7e'),
-(90, 12, 'dp0Xn5aW3T'),
-(91, 12, 'tCh7HtFfwf'),
-(92, 12, '4kWdFU8sRN'),
-(93, 12, 'CCWUtuRGGO'),
-(94, 12, 'aqSZlGQuzg'),
-(95, 12, 'owP1VdX8s4');
+(156, 13, 'sjtgYnNvX4'),
+(157, 13, 'nyINhVQUM8'),
+(158, 13, 'hvRBVSkn3Z'),
+(159, 13, '1jTEeGNjeB'),
+(160, 13, 'C4TunqRZQ0'),
+(161, 13, 'RTRFQFIMBXE8R'),
+(162, 13, 'RTRJ9YM3H4LJB'),
+(163, 13, 'RTRSWJ652Y9BY'),
+(164, 13, 'RTR25J0S1NHND'),
+(165, 13, 'RTRYLMJ6DKEO9'),
+(166, 13, 'RTRD66BOI1GCI'),
+(167, 13, 'RTR5A8JIDE2XE'),
+(168, 13, 'RTRGCTNCUARA9'),
+(169, 13, 'RTR3IHF68NNMA'),
+(170, 13, 'RTRW7ASLNOQRU');
 
 -- --------------------------------------------------------
 
@@ -494,15 +503,8 @@ CREATE TABLE `votes` (
 --
 
 INSERT INTO `votes` (`id`, `election_id`, `voters_id`, `candidate_id`, `position_id`, `timestamp`) VALUES
-(82, 12, 89, 24, 12, '2025-03-02 18:43:25'),
-(83, 12, 90, 25, 1, '2025-03-04 22:38:33'),
-(84, 12, 90, 26, 1, '2025-03-04 22:38:33'),
-(85, 12, 91, 25, 1, '2025-03-04 22:39:45'),
-(86, 12, 91, 26, 1, '2025-03-04 22:39:45'),
-(87, 12, 92, 25, 1, '2025-03-04 23:00:20'),
-(88, 12, 93, 25, 1, '2025-03-04 23:04:55'),
-(90, 12, 94, 25, 1, '2025-03-04 23:07:50'),
-(91, 12, 95, 25, 1, '2025-03-04 23:16:35');
+(92, 13, 156, NULL, 2, '2025-03-05 20:47:15'),
+(93, 13, 157, 29, 2, '2025-03-05 20:48:09');
 
 --
 -- Indexes for dumped tables
@@ -526,7 +528,8 @@ ALTER TABLE `cache_locks`
 ALTER TABLE `candidates`
   ADD PRIMARY KEY (`id`),
   ADD KEY `election_id` (`election_id`),
-  ADD KEY `position_id` (`position_id`);
+  ADD KEY `position_id` (`position_id`),
+  ADD KEY `partylist_id` (`partylist_id`);
 
 --
 -- Indexes for table `elections`
@@ -665,13 +668,13 @@ ALTER TABLE `votes`
 -- AUTO_INCREMENT for table `candidates`
 --
 ALTER TABLE `candidates`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
 
 --
 -- AUTO_INCREMENT for table `elections`
 --
 ALTER TABLE `elections`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT for table `failed_jobs`
@@ -683,13 +686,13 @@ ALTER TABLE `failed_jobs`
 -- AUTO_INCREMENT for table `feedback`
 --
 ALTER TABLE `feedback`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `history`
 --
 ALTER TABLE `history`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `history_candidates`
@@ -731,19 +734,19 @@ ALTER TABLE `migrations`
 -- AUTO_INCREMENT for table `partylists`
 --
 ALTER TABLE `partylists`
-  MODIFY `partylist_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `partylist_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `positions`
 --
 ALTER TABLE `positions`
-  MODIFY `position_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `position_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `students`
 --
 ALTER TABLE `students`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `users`
@@ -755,13 +758,13 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `voters`
 --
 ALTER TABLE `voters`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=96;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=171;
 
 --
 -- AUTO_INCREMENT for table `votes`
 --
 ALTER TABLE `votes`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=92;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=94;
 
 --
 -- Constraints for dumped tables
@@ -771,7 +774,8 @@ ALTER TABLE `votes`
 -- Constraints for table `candidates`
 --
 ALTER TABLE `candidates`
-  ADD CONSTRAINT `candidates_ibfk_1` FOREIGN KEY (`election_id`) REFERENCES `elections` (`id`);
+  ADD CONSTRAINT `candidates_ibfk_1` FOREIGN KEY (`election_id`) REFERENCES `elections` (`id`),
+  ADD CONSTRAINT `candidates_ibfk_3` FOREIGN KEY (`partylist_id`) REFERENCES `partylists` (`partylist_id`) ON DELETE SET NULL;
 
 --
 -- Constraints for table `feedback`
